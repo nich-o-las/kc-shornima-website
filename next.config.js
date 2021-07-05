f4670005c17b475b37c322adc2b2d7442ce2c15d
Nick Cox <nick.a.cox@gmail.com>
2021-07-05 01:35:34 -0500

    updates

--- next.config.js
+++ next.config.js
@@ -11,4 +11,12 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] },
+    });
+    return cfg;
+  },
 });
