c021c1e1c6eaddb9a080bf82292c1520b2c593c4
Nick Cox <nick.a.cox@gmail.com>
2021-07-06 01:00:51 -0500

    image optimization

--- next.config.js
+++ next.config.js
@@ -1,23 +1,30 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  baseUrl: '.',
-  paths: { '@': ['./src'] },
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    });
-    return config;
-  },
-});
+module.exports = withPlugins([
+  optimizedImages,
+  withBundleAnalyzer({
+    poweredByHeader: false,
+    trailingSlash: true,
+    basePath: '',
+    baseUrl: '.',
+    // TODO: Figure out how to properly set up this path for absolute imports
+    paths: { '@': ['./src'] },
+    // The starter code load resources from `public` folder with `router.basePath` in React components.
+    // So, the source code is "basePath-ready".
+    // You can remove `basePath` if you don't need it.
+    reactStrictMode: true,
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      });
+      return config;
+    },
+  }),
+]);
