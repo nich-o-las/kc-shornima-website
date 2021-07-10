b33611f3fae259693f500d80ba072aecfce61f7e
Nick Cox <nick.a.cox@gmail.com>
2021-07-09 22:34:24 -0500

    netlify config

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ module.exports = withBundleAnalyzer({
   trailingSlash: true,
   basePath: '',
   baseUrl: '.',
+  target: 'serverless',
   // TODO: Figure out how to properly set up this path for absolute imports
-  paths: { '@': ['./src'] },
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
